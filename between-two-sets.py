49002813f425aae39378425ebf9787fc0526288e
anirudhchandnani <69077653+anirudhchandnani@users.noreply.github.com>
2020-11-04 02:03:21 +0530

    Update between-two-sets.py

--- between-two-sets.py
+++ between-two-sets.py
@@ -6,13 +6,13 @@ def getTotalX(a, b):
     gcd_num = b[0]
     if len(a) > 1:
         for x in range(1,len(a)):
-            lcm_num =  (lcm_num * a[x])/gcd(lcm_num,a[x])
+            lcm_num =  (lcm_num * a[x])//math.gcd(lcm_num,a[x])
     if len(b) > 1:
         for x in range(1,len(b)):
-            gcd_num = gcd(gcd_num,b[x])
+            gcd_num = math.gcd(gcd_num,b[x])
     count = 0
     for x in range(lcm_num, gcd_num+1, lcm_num):
-        if gcd(x, gcd_num) == x:
+        if math.gcd(x, gcd_num) == x:
             count += 1
     return count
 
